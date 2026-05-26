d321dba2a002343d353dd3af5e81352b8097fa37
SARANYA5704 <saranyan1904@gmail.com>
2026-05-26 12:36:45 +0530

    Copied content from another account

--- ast_test_repo/app.py
+++ ast_test_repo/app.py
@@ -1,8 +1,1 @@
-from src.controllers.user_controller import create_user
-
-def main():
-    user = {"name": "Durai", "email": "durai@test.com"}
-    create_user(user)
-
-if __name__ == "__main__":
-    main()
+"# My-Shuttle-Project" 
